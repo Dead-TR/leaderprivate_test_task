a263e81efb7e6f55ce048466dce2841ae9130354
DTR <t.terry.rice@gmail.com>
2020-09-06 00:25:52 +0300

    end desktop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,64 @@
-"use strict";
-var sqrArr = [1, 2, 3, 4, 5, 6];
-var sqrNum = function (array) {
-    return array.reduce(function (a, b, i, src) {
-        // tslint:disable-next-line: no-unused-expression
-        console.log((a, b, i, src));
-        return a;
-    });
-};
+'use strict';
+
+const sliderBox = document.querySelector('.slider');
+let slideId = 1;
+
+sliderBox.addEventListener('click', (event) => {
+  const sliding = () => {
+    const sliderItems = [...document.querySelectorAll('.slider__item')];
+
+    (slideId > 3) && (slideId = 1);
+    (slideId < 1) && (slideId = 3);
+
+    for (const item of sliderItems) {
+      item.className = `slider__item slider__item_active-${slideId}`
+    }
+  };
+
+  if (event.target.classList.contains('slider__arrow')) {
+    if (event.target.classList.contains('slider__arrow_left')) {
+      sliding(--slideId);
+    }
+
+    if (event.target.classList.contains('slider__arrow_right')) {
+      sliding(++slideId);
+    }
+  }
+});
+
+const newsList = document.querySelector('.news');
+
+newsList.addEventListener('click', (event) => {
+  if (event.target.closest('.author__hide')) {
+    const hiddenElements = event.target.firstElementChild;
+    const threeDots = event.target.closest('.author__hide');
+
+    if (threeDots.classList.contains('author__hide_link')) {
+      threeDots.style = 'background: none; cursor: default;';
+    } else {
+      threeDots.style = 'background: none; cursor: default;';
+      hiddenElements.style = 'display: flex;';
+    }
+  }
+});
+
+const dropdown = document.querySelector('.dropdown');
+
+dropdown.addEventListener('click', (event) => {
+  const ev = event.target;
+
+  if (ev.classList.contains('dropdown__title')) {
+    const dropContent = ev.nextElementSibling;
+
+    dropContent.classList.toggle('dropdown__list_active');
+
+    if (dropContent.classList.contains('dropdown__list_active')) {
+      ev.classList.remove('dropdown__title_noactive');
+      ev.classList.add('dropdown__title_active');
+    } else {
+      ev.classList.add('dropdown__title_noactive');
+      ev.classList.remove('dropdown__title_active');
+    }
+
+  }
+})
